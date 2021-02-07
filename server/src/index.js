91090cc0ac11f8ce9480972382cd69dafc857424
Barnes2197 <mauriceb2197@gmail.com>
2021-02-07 02:06:03 -0500

    Added install_deps.js to try and make it easier to install dependencies, changed the default port for the server from 3000 to 5000 and added concurrently so that we can run both the frontend and the backend at the same time

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const router = require('./routes/todoItem');
 
 // env variables
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
